3b88531b01e7731441e9e69ce6df3401cdf3bea3
Anmol Parande <parandea1.7@gmail.com>
2019-01-24 21:06:59 -0800

    Created the CascadeClassifier

---
+++ cascade.py
@@ -0,0 +1,58 @@
+from viola_jones import ViolaJones
+import pickle
+"""
+A Python implementation of the "Attentional Cascade" mentioned in 
+Viola, Paul, and Michael Jones. "Rapid object detection using a boosted cascade of simple features." Computer Vision and Pattern Recognition, 2001. CVPR 2001. Proceedings of the 2001 IEEE Computer Society Conference on. Vol. 1. IEEE, 2001.
+Works in both Python2 and Python3
+"""
+
+class CascadeClassifier():
+    def __init__(self, layers):
+        self.layers = layers
+        self.clfs = []
+
+    def train(self, training):
+        pos, neg = [], []
+        for ex in training:
+            if ex[1] == 1:
+                pos.append(ex)
+            else:
+                neg.append(ex)
+        
+        for feature_num in self.layers:
+            if len(neg) == 0:
+                print("Stopping early. FPR = 0")
+                break
+            clf = ViolaJones(T=feature_num)
+            clf.train(pos+neg, len(pos), len(neg))
+            self.clfs.append(clf)
+            false_positives = []
+            for ex in neg:
+                if self.classify(ex[0]) == 1:
+                    false_positives.append(ex)
+            neg = false_positives
+
+    def classify(self, image):
+        for clf in self.clfs:
+            if clf.classify(image) == 0:
+                return 0
+        return 1
+
+    def save(self, filename):
+        """
+        Saves the classifier to a pickle
+          Args:
+            filename: The name of the file (no file extension necessary)
+        """
+        with open(filename+".pkl", 'wb') as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(filename):
+        """
+        A static method which loads the classifier from a pickle
+          Args:
+            filename: The name of the file (no file extension necessary)
+        """
+        with open(filename+".pkl", 'rb') as f:
+            return pickle.load(f)
--- face_detection.py
+++ face_detection.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pickle
 from viola_jones import ViolaJones
+from cascade import CascadeClassifier
+import time
 
-def train(t):
+def train_viola(t):
     with open("training.pkl", 'rb') as f:
         training = pickle.load(f)
     clf = ViolaJones(T=t)
@@ -10,15 +12,53 @@ def train(t):
     evaluate(clf, training)
     clf.save(str(t))
 
-def test(filename):
+def test_viola(filename):
     with open("test.pkl", 'rb') as f:
         test = pickle.load(f)
     
     clf = ViolaJones.load(filename)
     evaluate(clf, test)
+
+def train_cascade(layers, filename="Cascade"):
+    with open("training.pkl", 'rb') as f:
+        training = pickle.load(f)
     
+    clf = CascadeClassifier(layers)
+    clf.train(training)
+    evaluate(clf, training)
+    clf.save(filename)
+
+def test_cascade(filename="Cascade"):
+    with open("test.pkl", "rb") as f:
+        test = pickle.load(f)
+    
+    clf = CascadeClassifier.load(filename)
+    evaluate(clf, test)
+
 def evaluate(clf, data):
     correct = 0
+    all_negatives, all_positives = 0, 0
+    true_negatives, false_negatives = 0, 0
+    true_positives, false_positives = 0, 0
+    classification_time = 0
+
     for x, y in data:
-        correct += 1 if clf.classify(x) == y else 0
-    print("Classified %d out of %d test examples" % (correct, len(data)))
+        if y == 1:
+            all_positives += 1
+        else:
+            all_negatives += 1
+
+        start = time.time()
+        prediction = clf.classify(x)
+        classification_time += time.time() - start
+        if prediction == 1 and y == 0:
+            false_positives += 1
+        if prediction == 0 and y == 1:
+            false_negatives += 1
+        
+        correct += 1 if prediction == y else 0
+    
+    print("False Positive Rate: %d/%d (%f)" % (false_positives, all_negatives, false_positives/all_negatives))
+    print("False Negative Rate: %d/%d (%f)" % (false_negatives, all_positives, false_negatives/all_positives))
+    print("Accuracy: %d/%d (%f)" % (correct, len(data), correct/len(data)))
+    print("Average Classification Time: %f" % (classification_time / len(data)))
